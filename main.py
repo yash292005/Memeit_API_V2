76deefeab86434e6d48fb3c56f217afb445085a1
yash292005 <yvardhan744@gmail.com>
2021-09-10 12:38:21 +0530

    Initial Commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
-
 import praw
 from flask import Flask, jsonify
+
 
 app = Flask(__name__)
 reddit = praw.Reddit(client_id='2roma39k4b7nKeLdK4vtTQ',
@@ -11,27 +11,36 @@ reddit = praw.Reddit(client_id='2roma39k4b7nKeLdK4vtTQ',
 
 @app.route('/<string:n>')
 def userInput(n):
-    subreddit = reddit.subreddit(n)
-    Main_Output = subreddit.hot(limit=70)
-    MainArray = []
-    for submissions in Main_Output:
-        meme = submissions.url
-        title = submissions.title
-        extension = meme[len(meme) - 3:].lower()
-        if "jpg" in extension or "png" in extension or "gif" in extension:
-            result = {
-                "subreddit": n,
-                "title": title,
-                "url": meme,
-            }
-            MainArray.append(result)
+    try:
+        subreddit = reddit.subreddit(n)
+        Main_Output = subreddit.hot(limit=70)
+        MainArray = []
+        for submissions in Main_Output:
+            meme = submissions.url
+            title = submissions.title
+            extension = meme[len(meme) - 3:].lower()
+            if "jpg" in extension or "png" in extension or "gif" in extension:
+                result = {
+                    "subreddit": n,
+                    "title": title,
+                    "url": meme
+                }
+                MainArray.append(result)
 
-    finalResult = {
+        finalResult = {
+            "code": 200,
             "count": 50,
             "memes": MainArray
         }
 
-    return jsonify(finalResult)
+        return jsonify(finalResult)
+    except Exception as e:
+
+        return {
+            "memes": 400,
+            "message": str(type(e)) + str(e),
+            "help": "Subreddit Doesn't Exist, Check if u spelled it correctly.."
+        }
 
 
 if __name__ == "__main__":
