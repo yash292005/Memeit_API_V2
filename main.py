38da88175244c790c41dd5dea7e7247b18eed0b0
yash292005 <yvardhan744@gmail.com>
2021-09-10 11:51:13 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+import os
+
+import praw
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+reddit = praw.Reddit(client_id='2roma39k4b7nKeLdK4vtTQ',
+                     client_secret='wnNaoA1-kcefBJEgwJyLkEKQ0tdG3w', password='GajarKaHalwa1947',
+                     user_agent='PrawTut', username='MemeitOfficial')
+
+
+@app.route('/<string:n>')
+def userInput(n):
+    subreddit = reddit.subreddit(n)
+    Main_Output = subreddit.hot(limit=70)
+    MainArray = []
+    for submissions in Main_Output:
+        meme = submissions.url
+        title = submissions.title
+        extension = meme[len(meme) - 3:].lower()
+        if "jpg" in extension or "png" in extension or "gif" in extension:
+            result = {
+                "subreddit": n,
+                "title": title,
+                "url": meme,
+            }
+            MainArray.append(result)
+
+    finalResult = {
+            "count": 50,
+            "memes": MainArray
+        }
+
+    return jsonify(finalResult)
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.debug = True
+    app.run(port=port)
