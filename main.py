7411123c4386e2124376f87551a5ff8f5a08d744
yash292005 <yvardhan744@gmail.com>
2021-09-22 11:35:13 +0530

    Initial Commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import praw
 from flask import Flask, jsonify
+
 """
 This API has been created by yash vardhan, to use in "project-memeit".
 This API is private and not meant to be use publicly in any other project.
@@ -17,34 +18,58 @@ reddit = praw.Reddit(client_id='2roma39k4b7nKeLdK4vtTQ',
 def userInput(n):
     try:
         subreddit = reddit.subreddit(n)
-        Main_Output = subreddit.hot(limit=70)
+        Main_Output = subreddit.hot(limit=50)
         MainArray = []
         for submissions in Main_Output:
             meme = submissions.url
             title = submissions.title
             extension = meme[len(meme) - 3:].lower()
-            vid_extension = meme[:9]
             if "jpg" in extension or "png" in extension or "gif" in extension:
+                _ = submissions.preview
                 result = {
-                    "audio_url": meme + "/DASH_audio.mp4",
                     "subreddit": n,
                     "title": title,
-                    "url": meme,
+                    "url": meme
                 }
-                MainArray.append(result)
-            elif "https://v" in vid_extension:
-                result = {
-                    "audio_url": meme + "/DASH_audio.mp4",
-                    "subreddit": n,
-                    "title": title,
-                    "url": meme + "/DASH_360.mp4"
-
-                }
-
                 MainArray.append(result)
         finalResult = {
             "code": 200,
             "count": 50,
+            "memes": MainArray
+        }
+
+        return jsonify(finalResult)
+    except Exception as e:
+
+        return {
+            "code": 400,
+            "message": str(type(e)) + str(e),
+            "help": "Subreddit Doesn't Exist, Check if u spelled it correctly.."
+        }
+
+
+@app.route('/<string:topic>/<int:num>')
+def VideoUserInput(topic, num):
+    try:
+        subreddit = reddit.subreddit(topic)
+        Main_Output = subreddit.hot(limit=num)
+        MainArray = []
+        for submissions in Main_Output:
+            meme = submissions.url
+            title = submissions.title
+            vid_extension = meme[:9]
+            if "https://v" in vid_extension:
+                _ = submissions.preview
+                result = {
+                    "audio_url": meme + "/DASH_audio.mp4",
+                    "subreddit": topic,
+                    "title": title,
+                    "url": meme + "/DASH_360.mp4"
+                }
+                MainArray.append(result)
+        finalResult = {
+            "code": 200,
+            "count": num,
             "memes": MainArray
         }
 
